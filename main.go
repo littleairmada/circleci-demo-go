b5c7a7195432f3f6f21a81e99ce9a692ad1f8937
Blair Gillam <littleairmada@users.noreply.github.com>
2018-01-13 19:23:37 -0500

    update for littleairmada

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/CircleCI-Public/circleci-demo-go/service"
+	"github.com/littleairmada/circleci-demo-go/service"
 	_ "github.com/mattes/migrate/driver/postgres"
 	"github.com/mattes/migrate/migrate"
 )
